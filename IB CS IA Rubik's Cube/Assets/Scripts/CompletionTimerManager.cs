48eae1d72dec4dcb774f316290e3542111d45946
NookRitzia <56681647+NookRitzia@users.noreply.github.com>
2021-10-03 11:48:21 -0400

    Added Timers Rotation
    Added Timer Script which shows a timer which also checks if the cube is in the completed state. Additionally, a new rotation manager script has been implemented allowed the user to rotate the cube, once the cube is not being rotated anymore, the cube will snap back to its default position.

---
+++ IB CS IA Rubik's Cube/Assets/Scripts/CompletionTimerManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CompletionTimerManager : MonoBehaviour
+{
+    [SerializeField] private Face[] faces;
+    [SerializeField] bool ifSolved;
+    [SerializeField] float timeElapsed = 0;
+    [SerializeField] float startTime;
+    [SerializeField] UnityEngine.UI.Text timerText;
+
+    private void Start()
+    {
+        faces = this.GetComponentsInChildren<Face>();
+        startTime = Time.time;
+    }
+
+    private void Update()
+    {
+        timeElapsed = Time.time - startTime + 50;
+        ifSolved = isCubeSolved();
+        Debug.Log(ifSolved);
+        setTimer();
+    }
+
+    public bool isCubeSolved()
+    {
+        foreach(Face f in faces)
+        {
+            string orientation = f.getOrientation();
+            foreach(Tile t in f.getTiles())
+                if (!t.getColorOrientation().Equals(orientation))
+                    return false;
+
+        }
+        return true;
+    }
+
+    private void setTimer()
+    {
+        if (timerText != null)
+        {
+            string minutes = Mathf.FloorToInt(timeElapsed / 60) + "";
+            string seconds = Mathf.FloorToInt(timeElapsed % 60) + "";
+            string ms = Mathf.FloorToInt((timeElapsed % 1f) * 1000) + "";
+            if (minutes.Length <= 1)
+                minutes = "0" + minutes;
+            if (seconds.Length <= 1)
+                seconds = "0" + seconds;
+            if (ms.Length <= 1)
+                ms = "0" + ms;
+            ms = ms.Substring(0, 2);
+            timerText.text = minutes + ":" + seconds + ":" + ms;
+            Debug.Log(ms);
+        }
+    }
+}
